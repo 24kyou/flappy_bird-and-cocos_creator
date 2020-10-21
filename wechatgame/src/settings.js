8143238617fa4086ce89071bcf7da086e7f4d297
24kyou <54715120+24kyou@users.noreply.github.com>
2020-10-21 23:54:43 +0800

    Add files via upload

---
+++ wechatgame/src/settings.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"wechatgame",groupList:["default","bird","block"],collisionMatrix:[[false],[false,false,true],[false,true,false]],hasResourcesBundle:false,hasStartSceneBundle:true,remoteBundles:[],subpackages:[],launchScene:"db://assets/scene/Title.fire",orientation:"",server:"",jsList:[],bundleVers:{internal:"18db7","start-scene":"4345e",main:"4aff1"}};
